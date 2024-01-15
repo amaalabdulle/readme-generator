c3c9729d52e160cdbf4b1c12f8c0bbcafcdc63fb
amaal abdulle <amaalabdelmuid@gmail.com>
2024-01-15 09:36:49 +0000

    title, description and contents added

--- index.js
+++ index.js
@@ -5,13 +5,40 @@ const inquirer = require("inquirer");
 
 
 // function to write README file
-function writeToFile() {
-    
+function writeToFile(title, description, contents) {
+
+
+    const content = `# ${title}\n\n## Description\n${description}\n\n## Contents\n${contents}`;
+    fs.appendFile('README.md', content, (err) =>
+        err ? console.error(err) : console.log('Commit logged!')
+    );
 }
 
 // function to initialize program
 function init() {
     inquirer
+        .createPromptModule([
+            {
+            type: 'input',
+            message: 'What is title of your project?',
+            name: 'title',
+            },
+            {
+            type: 'input',
+            message: 'Write a description about your project',
+            name: 'description',
+            },
+            {
+            type: 'input',
+            message: 'Write your table of contents (separate items with commas)',
+            name: 'contents',
+            },
+        ])
+
+        .then((responses) => {
+            const { title, description, contents} = responses;
+            writeToFile(title, description, contents);
+        });
 }
 
 // function call to initialize program
