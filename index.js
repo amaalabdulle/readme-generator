82722402a47c32c3e46c3213ed868c6d05da57cd
amaal abdulle <amaalabdelmuid@gmail.com>
2024-01-15 09:34:07 +0000

    added original functions

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+// const path = require('path');
+const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown");
+
+
+// function to write README file
+function writeToFile() {
+    
+}
+
+// function to initialize program
+function init() {
+    inquirer
+}
+
+// function call to initialize program
+init();
